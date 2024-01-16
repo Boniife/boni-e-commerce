e2674d2fa10fa63777f7e9473460ddbec8a147a0
Boniife <anyanwuifeanyi3@gmail.com>
2024-01-16 13:42:01 +0100

    product update

--- src/app/(pages)/products/Filters/index.tsx
+++ src/app/(pages)/products/Filters/index.tsx
@@ -1,9 +1,37 @@
+ 'use client'
+
  import React from 'react'
  
- const Filters = () => {
+ import classes from './index.module.scss'
+
+import { useFilter } from '../../../_providers/Filter'
+import { Category } from '../../../../payload/payload-types';
+
+ const Filters = ({categories}: {categories: Category[]}) => {
+  const { categoryFilters, sort, setCategoryFilters, setSort} = useFilter();
+
+  const handleCategories = (categoryId: string) => {}
+
    return (
-     <div>
-       filters
+     <div className={classes.filter}>
+       <div>
+        <h6 className={classes.title}>Product Categories</h6>
+        <div className={classes.categories}>
+          {categories.map((category) => {
+            const isSelected = false
+
+//             return (
+//               <Checkbox 
+//             key={category.id} 
+//             label={category.title} 
+//             value={category.id}
+//             isSelected={isSelected}
+//             onClickHandler={handleCategories}
+//             />
+// )
+          })}
+        </div>
+       </div>
      </div>
    )
  }
